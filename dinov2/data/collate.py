fddd3ac1353555f80678d45def2b709b28f6f2bb
ChrisGMagnitude <c.griffiths@magnitudesurveys.co.uk>
2025-05-12 16:19:31 +0100

    ...

--- dinov2/data/collate.py
+++ dinov2/data/collate.py
@@ -10,7 +10,7 @@ import random
 def collate_data_and_cast(samples_list, mask_ratio_tuple, mask_probability, dtype, n_tokens=None, mask_generator=None):
     # dtype = torch.half  # TODO: Remove
 
-    print('samples_list',samples_list)
+    print('samples_list',samples_list.shape)
     n_global_crops = len(samples_list[0][0]["global_crops"])
     n_local_crops = len(samples_list[0][0]["local_crops"])
 
