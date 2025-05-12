f6f7b32989453980ec5059b77142a9b846a4a875
ChrisGMagnitude <c.griffiths@magnitudesurveys.co.uk>
2025-05-12 11:33:49 +0100

    ...

--- run_dinov2.py
+++ run_dinov2.py
@@ -1,5 +1,8 @@
 from dinov2.data.datasets import ImageNet
 
+
+data_root = r'/root/dinov2_data'
+
 for split in ImageNet.Split:
-    dataset = ImageNet(split=split, root="<ROOT>", extra="<EXTRA>")
+    dataset = ImageNet(split=split, root=data_root, extra=data_root)
     dataset.dump_extra()
