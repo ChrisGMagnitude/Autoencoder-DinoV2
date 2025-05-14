71efbc684e227ca2fd4fffd1100aa5e7c553dbd3
ChrisGMagnitude <c.griffiths@magnitudesurveys.co.uk>
2025-05-14 15:31:53 +0100

    remove gausiannoise

--- dinov2/data/datasets/magdata.py
+++ dinov2/data/datasets/magdata.py
@@ -128,7 +128,7 @@ class MagData(ExtendedVisionDataset):
                                             v2.CenterCrop((crop_size,crop_size)),
                                             v2.RandomRotation(degrees=(0, 360)),
                                             v2.RandomHorizontalFlip(),
-                                            v2.GaussianNoise(sigma=rand)
+                                            #v2.GaussianNoise(sigma=rand)
                                             ])
         else:
             transformer = Compose([
